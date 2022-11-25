1dc98ce135af84ae2b64edfb689b501b7066e327
gabo <sfredfmex@gmail.com>
2022-11-25 15:23:10 -0600

    add your commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>
+            Pagina Git
+        </title>
+    </head>
+
+<body>
+    
+
+    <?php 
+    echo   '<a> Operaciones Aritmeticas Git Ejercicios </a>';
+    echo '<br> </br>';
+    $valor1 = 5;
+    $valor2 = 3;
+    
+    $suma = $valor1 + $valor2;
+    
+    echo '<a> La suma es: </a>', $suma;
+     ?>
+</body>
+
+</html>
